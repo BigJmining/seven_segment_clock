beff868a0fede2bf767a91b75f09bdc23940fe1b
Jason <jlohrey09@gmail.com>
2022-12-05 21:07:22 -0800

    completed source code

--- src/theClock.py
+++ src/theClock.py
@@ -12,156 +12,135 @@ cwd = os.getcwd()
 
 clock = pygame.time.Clock()
 
-SCREEN_WIDTH = 375
+SCREEN_WIDTH = 1300
 SCREEN_HEIGHT = 450
 
 window = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption("Seven Segment Clock")
 
-BG_color = (0,0,0)
-seg_color = (255,0,0)
 
 clock_face = pygame.image.load(os.path.join(cwd,'images','clock_face.png')).convert_alpha()
 clock_hand_vert = pygame.image.load(os.path.join(cwd,'images','clock_hand.png')).convert_alpha()
 clock_hand_horz = pygame.transform.rotate(clock_hand_vert,90)
 clock_hand_dot = pygame.image.load(os.path.join(cwd,'images','clock_dot.png')).convert_alpha()
 
+OFFSET = 285
+GAP = 60
+FPS = 60
 
 class CLOCK:
     def __init__(self):
         self.name = 'clock'
-        self.segments_count = 7
-        self.seg_1 = False
-        self.seg_2 = False
-        self.seg_3 = False
-        self.seg_4 = False
-        self.seg_5 = False
-        self.seg_6 = False
-        self.seg_7 = False
-        self.seg_dot = False
+        
 
     def pass_the_time(self):
         
-        time_now = (datetime.datetime.now().strftime("%S"))
-        
-        # digits = time_now.split()
-        # print(digits, time_now)
-        # hour1 = digits[0][-1]
-        hour1 = time_now[-1]
-        if hour1 == "1":
-            self.seg_1 = 0
-            self.seg_2 = 0
-            self.seg_3 = 0
-            self.seg_4 = 0
-            self.seg_5 = 0
-            self.seg_6 = 1
-            self.seg_7 = 1
-            self.seg_dot = 0
-        elif hour1 == "2":
-            self.seg_1 = 0
-            self.seg_2 = 1
-            self.seg_3 = 1
-            self.seg_4 = 1
-            self.seg_5 = 1
-            self.seg_6 = 1
-            self.seg_7 = 0
-            self.seg_dot = 0
-        elif hour1 == "3":
-            self.seg_1 = 0
-            self.seg_2 = 0
-            self.seg_3 = 1
-            self.seg_4 = 1
-            self.seg_5 = 1
-            self.seg_6 = 1
-            self.seg_7 = 1
-            self.seg_dot = 0
-        elif hour1 == "4":
-            self.seg_1 = 1
-            self.seg_2 = 0
-            self.seg_3 = 0
-            self.seg_4 = 1
-            self.seg_5 = 0
-            self.seg_6 = 1
-            self.seg_7 = 1
-            self.seg_dot = 0
-        elif hour1 == "5":
-            self.seg_1 = 1
-            self.seg_2 = 0
-            self.seg_3 = 1
-            self.seg_4 = 1
-            self.seg_5 = 1
-            self.seg_6 = 0
-            self.seg_7 = 1
-            self.seg_dot = 0
-        elif hour1 == "6":
-            self.seg_1 = 1
-            self.seg_2 = 1
-            self.seg_3 = 1
-            self.seg_4 = 1
-            self.seg_5 = 1
-            self.seg_6 = 0
-            self.seg_7 = 1
-            self.seg_dot = 0
-        elif hour1 == "7":
-            self.seg_1 = 0
-            self.seg_2 = 0
-            self.seg_3 = 1
-            self.seg_4 = 0
-            self.seg_5 = 0
-            self.seg_6 = 1
-            self.seg_7 = 1
-            self.seg_dot = 0
-        elif hour1 == "8":
-            self.seg_1 = 1
-            self.seg_2 = 1
-            self.seg_3 = 1
-            self.seg_4 = 1
-            self.seg_5 = 1
-            self.seg_6 = 1
-            self.seg_7 = 1
-            self.seg_dot = 0
-        elif hour1 == "9":
-            self.seg_1 = 1
-            self.seg_2 = 0
-            self.seg_3 = 1
-            self.seg_4 = 1
-            self.seg_5 = 1
-            self.seg_6 = 1
-            self.seg_7 = 1
-            self.seg_dot = 0
-        elif hour1 == "0":
-            self.seg_1 = 1
-            self.seg_2 = 1
-            self.seg_3 = 1
-            self.seg_4 = 0
-            self.seg_5 = 1
-            self.seg_6 = 1
-            self.seg_7 = 1
-            self.seg_dot = 1
-        return
+        time_now = (datetime.datetime.now().strftime("%H%M"))
+        print(time_now)
+       
+        digit_1 = time_now[0]
+        digit_2 = time_now[1]
+        digit_3 = time_now[2]
+        digit_4 = time_now[3]
+        # digit_5 = time_now[3]
+        # digit_6 = time_now[3]
+        digits = [digit_1, digit_2, digit_3, digit_4]#, digit_5, digit_6]
+       
+        clock_digits = {
+                '1': [0,0,0,0,0,1,1,0],
+                '2': [0,1,1,1,1,1,0,0], 
+                '3': [0,0,1,1,1,1,1,0],
+                '4': [1,0,0,1,0,1,1,0],
+                '5': [1,0,1,1,1,0,1,0],
+                '6': [1,1,1,1,1,0,1,0],
+                '7': [0,0,1,0,0,1,1,0],
+                '8': [1,1,1,1,1,1,1,0],
+                '9': [1,0,1,1,1,1,1,0],
+                '0': [1,1,1,0,1,1,1,0]}
+
+        digits_to_display = [clock_digits[digits[x]] for x in range(len(digits))]
+            
+        return digits_to_display    
 
 
     def draw(self,window):
         window.blit(clock_face,(0,0))
-        self.pass_the_time()
+
+        digits = self.pass_the_time()
+        # print(digits)
         
-        if (self.seg_1):
-            window.blit(clock_hand_vert,(50,50))    # left top
-        if (self.seg_2):
-            window.blit(clock_hand_vert,(50,250))   # left btm
-        if (self.seg_3):
-            window.blit(clock_hand_horz,(110,15))   # top
-        if (self.seg_4):
-            window.blit(clock_hand_horz,(110,200))   # middle
-        if (self.seg_5):
-            window.blit(clock_hand_horz,(110,385))   # btm
-        if (self.seg_6):
-            window.blit(clock_hand_vert,(270,50))   # right top
-        if (self.seg_7):
-            window.blit(clock_hand_vert,(270,250))   # right btm
-        if (self.seg_dot):
-            window.blit(clock_hand_dot,(322,390))   # dot
-       
+        if digits[0][0]:
+            window.blit(clock_hand_vert,(50,50))                # left top
+        if digits[0][1]:
+            window.blit(clock_hand_vert,(50,250))               # left btm
+        if digits[0][2]:
+            window.blit(clock_hand_horz,(110,15))               # top
+        if digits[0][3]:
+            window.blit(clock_hand_horz,(110,200))               # middle
+        if digits[0][4]:
+            window.blit(clock_hand_horz,(110,385))              # btm
+        if digits[0][5]:
+            window.blit(clock_hand_vert,(270,50))               # right top
+        if digits[0][6]:
+            window.blit(clock_hand_vert,(270,250))              # right btm
+        if digits[0][7]:
+            window.blit(clock_hand_dot,(322,390))               # dot
+
+        if digits[1][0]:
+            window.blit(clock_hand_vert,(50+OFFSET,50))         # left top
+        if digits[1][1]:
+            window.blit(clock_hand_vert,(50+OFFSET,250))        # left btm
+        if digits[1][2]:
+            window.blit(clock_hand_horz,(110+OFFSET,15))        # top
+        if digits[1][3]:
+            window.blit(clock_hand_horz,(110+OFFSET,200))        # middle
+        if digits[1][4]:
+            window.blit(clock_hand_horz,(110+OFFSET,385))       # btm
+        if digits[1][5]:
+            window.blit(clock_hand_vert,(270+OFFSET,50))        # right top
+        if digits[1][6]:
+            window.blit(clock_hand_vert,(270+OFFSET,250))       # right btm
+        if digits[1][7]:
+            window.blit(clock_hand_dot,(322+OFFSET,390))        # dot
+
+        pygame.draw.circle(window,(111,111,111),(SCREEN_WIDTH //2 -12,125),20)
+        pygame.draw.circle(window,(111,111,111),(SCREEN_WIDTH //2 -12,275),20)
+
+        if digits[2][0]:
+            window.blit(clock_hand_vert,(50+GAP+(OFFSET*2),50))     # left top
+        if digits[2][1]:
+            window.blit(clock_hand_vert,(50+GAP+(OFFSET*2),250))    # left btm
+        if digits[2][2]:
+            window.blit(clock_hand_horz,(110+GAP+(OFFSET*2),15))    # top
+        if digits[2][3]:
+            window.blit(clock_hand_horz,(110+GAP+(OFFSET*2),200))    # middle
+        if digits[2][4]:
+            window.blit(clock_hand_horz,(110+GAP+(OFFSET*2),385))   # btm
+        if digits[2][5]:
+            window.blit(clock_hand_vert,(270+GAP+(OFFSET*2),50))    # right top
+        if digits[2][6]:
+            window.blit(clock_hand_vert,(270+GAP+(OFFSET*2),250))   # right btm
+        if digits[2][7] :
+            window.blit(clock_hand_dot,(322+GAP+(OFFSET*2),390))    # dot    
+
+        if digits[3][0]:
+            window.blit(clock_hand_vert,(50+GAP+(OFFSET*3),50))     # left top
+        if digits[3][1]:
+            window.blit(clock_hand_vert,(50+GAP+(OFFSET*3),250))    # left btm
+        if digits[3][2]:
+            window.blit(clock_hand_horz,(110+GAP+(OFFSET*3),15))    # top
+        if digits[3][3]:
+            window.blit(clock_hand_horz,(110+GAP+(OFFSET*3),200))    # middle
+        if digits[3][4]:
+            window.blit(clock_hand_horz,(110+GAP+(OFFSET*3),385))   # btm
+        if digits[3][5]:
+            window.blit(clock_hand_vert,(270+GAP+(OFFSET*3),50))    # right top
+        if digits[3][6]:
+            window.blit(clock_hand_vert,(270+GAP+(OFFSET*3),250))   # right btm
+        if digits[3][7]:
+            window.blit(clock_hand_dot,(322+GAP+(OFFSET*3),390))    # dot
+
+        
         pygame.display.update()    
 
 def redrawscreen():
@@ -172,15 +151,10 @@ C = CLOCK()
 
 run = True
 while run:
-    clock.tick(27)
+    clock.tick(FPS)
     
     # event detection
     keys = pygame.key.get_pressed()
-    
-    if keys[pygame.K_g]:
-        print('GO key pressed')
-    if keys[pygame.K_s]:
-        print('STOP key pressed')
 
     if keys[pygame.K_q]:
         run = False
